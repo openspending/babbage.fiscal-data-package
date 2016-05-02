48c3d8dda694a531483396fadf2cb6cff0668bba
Adam Kariv <adam.kariv@gmail.com>
2016-05-02 16:05:35 +0300

    Provide better status indications while publishing

--- babbage_fiscal/tasks.py
+++ babbage_fiscal/tasks.py
@@ -2,44 +2,49 @@ import json
 import traceback
 
 from celery import Celery
-import requests
 
+from babbage_fiscal.callbacks import STATUS_LOADING_DATA
 from .config import get_engine, _set_connection_string
 from .loader import FDPLoader
+from .callbacks import do_request, STATUS_INITIALIZING, STATUS_DONE, STATUS_FAIL
 
 app = Celery('fdp_loader')
 app.config_from_object('babbage_fiscal.celeryconfig')
 
 
-def do_request(callback, package, status, progress=None, error=None, data=None):
-    params = {'package': package, 'status': status}
-    if progress is not None:
-        params['progress'] = progress
-    if error is not None:
-        params['error'] = error
-    if data is not None:
-        params['data'] = json.dumps(data, indent=0)
-    requests.get(callback, params).content
+class ProgressSender(object):
+
+    def __init__(self, callback, package):
+        self.count = 0
+        self.callback = callback
+        self.package = package
+
+    def __call__(self, status=STATUS_LOADING_DATA, count=None, data=None):
+        if count is None:
+            count = self.count
+        else:
+            self.count = count
+        do_request(self.callback, self.package, status, count, data)
+
 
 @app.task
 def load_fdp_task(package, callback, connection_string=None):
-    def send_progress(status='progress', count=0, data=None):
-        do_request(callback, package, status, count, data)
+    send_progress = ProgressSender(callback, package)
     if connection_string is not None:
         _set_connection_string(connection_string)
     try:
         print("Starting to load %s" % package)
-        send_progress(status='starting')
+        send_progress(status=STATUS_INITIALIZING)
         model_name, package_contents, model = FDPLoader.load_fdp_to_db(package, get_engine(), send_progress)
         response = {
             'model_name': model_name,
             'babbage_model': model,
             'package': package_contents
         }
-        send_progress(status='done', data=response)
+        send_progress(status=STATUS_DONE, data=response)
         print("Finished to load %s" % package)
     except Exception as e:
-        do_request(callback, package, 'fail', error=repr(e))
+        do_request(callback, package, STATUS_FAIL, error=repr(e))
         traceback.print_exc()
         print("Failed to load %s: %r" % (package, e))
 
